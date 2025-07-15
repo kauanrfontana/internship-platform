0f87bf690bd61aba70eb7cf74b7e354e7d426751
Kauan Ramos Fontana <kauanrfontana@gmail.com>
2025-07-15 20:21:03 -0300

    fix: remove unused variable

--- src/pages/app/home.tsx
+++ src/pages/app/home.tsx
@@ -93,12 +93,6 @@ function mapJsonToInternship(jsonItem: any, id: number): InternshipWithExtra {
 // Remove o mockNews e passa a usar o noticiasData importado
 const news = noticiasData;
 
-const statusColors: Record<Internship['status'], string> = {
-  'Em andamento': 'bg-yellow-100 text-yellow-800',
-  'Concluído': 'bg-green-100 text-green-800',
-  'Pendente': 'bg-red-100 text-red-800',
-}
-
 const initialAllInternships: InternshipWithExtra[] = registroDeEstagiosData.map((item: any, index: number) => mapJsonToInternship(item, index + 1));
 
 export function Home() {
