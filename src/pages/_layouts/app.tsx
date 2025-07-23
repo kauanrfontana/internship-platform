b6c73a5ad092334f787ec5836379eb20713e93e7
Kauan Ramos Fontana <kauanrfontana@gmai.com>
2025-07-22 21:47:28 -0300

    feat: update

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -4,7 +4,12 @@ import { Outlet } from "react-router-dom"
 export function AppLayout() {
     return (
         <div className="flex min-h-screen flex-col">
-            <Header/>
+            <div className="text-center opacity-50 fixed bottom-0 left-1/2 -translate-x-1/2 z-50 bg-black text-white px-4 py-2 rounded-b shadow-md">
+                Este é um protótipo, não o sistema final! <br/>
+                Avalie sua experiência, como se fosse um sistema final: <a href="https://forms.gle/3GSZBpngb2aoh7Sm6">Aqui</a>
+            </div>
+            <Header />
+
 
             <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
                 <Outlet />
--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -6,6 +6,10 @@ import { FrequentlyAskedQuestions } from "../app/frequently-asked-questions";
 export function AuthLayout() {
     return (
         <div className="min-h-screen grid grid-cols-2 antialiased">
+            <div className="text-center fixed bottom-0 opacity-50 left-1/2 -translate-x-1/2 z-50 bg-black text-white px-4 py-2 rounded-b shadow-md">
+                Este é um protótipo, não o sistema final! <br/>
+                Avalie sua experiência, como se fosse um sistema final: <a href="https://forms.gle/3GSZBpngb2aoh7Sm6">Aqui</a>
+            </div>
             <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
                 <div className="flex items-center gap-3 text-lg font-medium text-foreground">
                     <GraduationCap className="h-5 w-5"/>
